ccc33435a64c9ad2b4f3b2e9948415cdf581aa38
Dillon Huff <dillonhuff@gmail.com>
2017-05-27 09:38:11 -0700

    Can now compute symbolic roots

--- Linear.hs
+++ Linear.hs
@@ -6,7 +6,18 @@ import Data.List
 data LinearExpr = LinearExpr [(Rational, String)] Rational
                   deriving (Eq, Ord, Show)
 
-mkLinear terms const = LinearExpr terms const
+
+sumCoeffs :: [(Rational, String)] -> (Rational, String)
+sumCoeffs [] = error "sumCoeffs empty"
+sumCoeffs cs =
+  let v = snd $ head cs in
+   (foldr (+) 0 $ map fst cs, v)
+
+addLikeTerms a =
+  let grouped = groupBy (\(_, a) (_, b) -> a == b)$ sortBy (\(_, b) (_, d) -> compare b d) a in
+   map sumCoeffs grouped
+
+mkLinear terms const = LinearExpr (filter (\(a, _) -> a /= 0) $ addLikeTerms terms) const
 
 scalarTimes :: Rational -> LinearExpr -> LinearExpr
 scalarTimes r (LinearExpr terms c) =
@@ -16,9 +27,14 @@ getTerm :: String -> LinearExpr -> (Rational, String)
 getTerm varName (LinearExpr terms _) =
   fromJust $ find (\(a, b) -> b == varName) terms
 
-minus :: LinearExpr -> LinearExpr -> LinearExpr
-minus (LinearExpr a c) (LinearExpr q r) =
-  error "sub"
+   
+plus :: LinearExpr -> LinearExpr -> LinearExpr
+plus (LinearExpr a c) (LinearExpr q r) =
+  let newC = c - r
+      newTerms = a ++ q in
+   mkLinear newTerms newC
+
+minus a b = plus a (scalarTimes (-1) b)
 
 symRoot :: String -> LinearExpr -> LinearExpr
 symRoot varName linExpr =
