bda6c418a3dbbcfb901dbe0a1db62859a2a296a6
Dillon Huff <dillonhuff@gmail.com>
2017-06-13 19:24:13 -0700

    Adding substitution

--- FerranteRackoff.hs
+++ FerranteRackoff.hs
@@ -2,10 +2,25 @@ module FerranteRackoff where
 
 import Data.List
 
+import Elimination
 import Linear
 import Logic
 
-ferranteRackoff :: Formula LinearExpr -> Formula LinearExpr
-ferranteRackoff f =
-  let fs = nub $ collectFormulas f in
-   f
+substituteForVar :: String -> LinearExpr -> Formula LinearExpr -> Formula LinearExpr
+substituteForVar varName e f = f
+
+buildFRTestPoints :: String -> [LinearExpr] -> [LinearExpr]
+buildFRTestPoints varName fs =
+  let symRoots = map (symRoot varName) fs
+      -- Need to filter out identical cases
+      midPoints = [scalarTimes (1 / 2) (plus a b) | a <- symRoots, b <- symRoots]
+      one = mkLinear [] 1
+      negInfPoints = map (minus one) symRoots 
+      posInfPoints = map (plus one) symRoots in
+   symRoots ++ midPoints ++ negInfPoints ++ posInfPoints
+
+ferranteRackoff :: String -> Formula LinearExpr -> Formula LinearExpr
+ferranteRackoff s f =
+  let fs = nub $ collectFormulas f
+      testPoints = buildFRTestPoints s fs in
+   foldOrs $ map (\a -> substituteForVar s a f) testPoints
