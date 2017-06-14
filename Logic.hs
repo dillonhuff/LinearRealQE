be32946079286a580f2cdd4da680f6588d56a579
Dillon Huff <dillonhuff@gmail.com>
2017-06-13 20:25:16 -0700

    Added better formula showing

--- Logic.hs
+++ Logic.hs
@@ -16,3 +16,12 @@ collectFormulas (Or l r) = (collectFormulas l) ++ (collectFormulas r)
 collectFormulas (Not l) = (collectFormulas l)
 collectFormulas (Atom _ l) = [l]
 collectFormulas _ = []
+
+showFm (And a b) = "(" ++ (showFm a) ++ " ^ " ++ (showFm b) ++ ")"
+showFm (Or a b) = "(" ++ showFm a ++ " v " ++ showFm b ++ ")"
+showFm (Not a) = "~(" ++ showFm a ++ ")"
+showFm (Atom EQL a) = "(" ++ show a ++ " = 0)"
+showFm (Atom LESS a) = "(" ++ show a ++ " < 0)"
+showFm (Atom GREATER a) = "(" ++ show a ++ " > 0)"
+showFm T = "True"
+showFm F = "False"
